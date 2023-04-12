04cfe4d86649a558092bda8b08f3ecc13e45ff3e
Harsha <santoshsriharsha@gmail.com>
2023-04-12 21:54:34 +0530

    Add two way binding

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -17,29 +17,51 @@ const ExpenseForm = () => {
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
   };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+
+    console.log(expenseData);
+  };
   return (
-    <div className="new-expense__controls">
-      <div className="new-expense__control">
-        <label>Title</label>
-        <input type="text" onChange={titleChangeHandler} />
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type="date" value={enteredDate} onChange={dateChangeHandler} />
+        </div>
+        <button type="submit" className="new-expense__actions">
+          Add Expense
+        </button>
       </div>
-      <div className="new-expense__control">
-        <label>Amount</label>
-        <input
-          type="number"
-          min="0.01"
-          step="0.01"
-          onChange={amountChangeHandler}
-        />
-      </div>
-      <div className="new-expense__control">
-        <label>Date</label>
-        <input type="date" onChange={dateChangeHandler} />
-      </div>
-      <button type="submit" className="new-expense__actions">
-        Add Expense
-      </button>
-    </div>
+    </form>
   );
 };
 
