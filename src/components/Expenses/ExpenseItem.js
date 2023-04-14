c4bfa1eccc163c8c3fc7b77f463b43a22915b8bc
Harsha <santoshsriharsha@gmail.com>
2023-04-14 10:28:49 +0530

    Improve the semantics of ExpensesList component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -8,13 +8,15 @@ function ExpenseItem(props) {
   const expenseAmount = props.amount;
 
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={expenseDate} />
-      <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">₹{expenseAmount}</div>
-      </div>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={expenseDate} />
+        <div className="expense-item__description">
+          <h2>{expenseTitle}</h2>
+          <div className="expense-item__price">₹{expenseAmount}</div>
+        </div>
+      </Card>
+    </li>
   );
 }
 
